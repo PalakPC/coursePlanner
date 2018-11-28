84f39d4de01d280f2e4054262b91ac1e04aff0d4
pchoudhary32 <pchoudhary32@gatech.edu>
2018-11-28 18:58:29 -0500

    html pages

--- app.js
+++ app.js
@@ -9,28 +9,31 @@ var app = express();
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
-    database: 'files'
+    password: 'password',
+    database: 'coursePlanner'
 });
 connection.connect();
 
+require("./config/express.js")(app, config);
+
 app.get('/', function(req, res) {
-    res.redirect("login");
-});
+        res.redirect("login");
+    });
 
 //Serving pages
 app.get("/login", mysqlController.loginPage);
-app.get("/register", mysqlController.helper.authenticateAdmin, mysqlController.registerPage);
+/*app.get("/register", mysqlController.helper.authenticateAdmin, mysqlController.registerPage);
 app.get("/home", mysqlController.helper.authenticate, mysqlController.home);
-
+app.get("/inputQuery", mysqlController.inputQuery);
+*/
 //test page
 app.get("/testPage", mysqlController.testPage);
-
+/*
 //Posts
 app.post("/login", mysqlController.login);
 app.post("/logout", mysqlController.logout);
 app.post("/register", mysqlController.helper.authenticateAdmin, mysqlController.register);
-
+*/
 app.listen(config.SERVER_PORT);
 console.log("mysql server up and running at --> " + config.SERVER_PORT);
 
--- controllers/mysql.js
+++ controllers/mysql.js
@@ -1,2 +1,13 @@
-var User = require("../models/user.js");
+//var User = require("../models/user.js");
+var mysql = require("mysql");
+var childProcess = require("child_process");
+var url = require("url");
+var queryString = require("querystring");
 
+var mysqlController = {
+    testPage: function(req, res) {
+		        res.sendfile("testPage.html", {root: "./views/"});
+	          },
+};
+
+module.exports = mysqlController;
