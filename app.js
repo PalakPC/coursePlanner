6bed5d0b9621a9211929babe223340be6e14e25e
pchoudhary32 <pchoudhary32@gatech.edu>
2018-11-28 15:09:18 -0500

    initial

---
+++ app.js
@@ -0,0 +1,36 @@
+var mysql = require("mysql");
+var express = require("express");
+var config = require("./config/config.js")
+var mysqlController = require("./controllers/mysql.js");
+var _ = require("underscore");
+
+var app = express();
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'files'
+});
+connection.connect();
+
+app.get('/', function(req, res) {
+    res.redirect("login");
+});
+
+//Serving pages
+app.get("/login", mysqlController.loginPage);
+app.get("/register", mysqlController.helper.authenticateAdmin, mysqlController.registerPage);
+app.get("/home", mysqlController.helper.authenticate, mysqlController.home);
+
+//test page
+app.get("/testPage", mysqlController.testPage);
+
+//Posts
+app.post("/login", mysqlController.login);
+app.post("/logout", mysqlController.logout);
+app.post("/register", mysqlController.helper.authenticateAdmin, mysqlController.register);
+
+app.listen(config.SERVER_PORT);
+console.log("mysql server up and running at --> " + config.SERVER_PORT);
+
---
+++ config/config.js
@@ -0,0 +1,6 @@
+var path = require("path");
+
+module.exports = {
+    SERVER_PORT: 8888,
+    ROOT_PATH: path.normalize(__dirname + "/..")
+};
